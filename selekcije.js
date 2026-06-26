329fcd63ce2086df651b4ea0a79eb47fb7daba47
KK TEMPO BRATUNAC <davidiliic996@gmail.com>
2026-06-26 09:54:51 +0200

    Add files via upload

---
+++ selekcije.js
@@ -0,0 +1,15 @@
+function otvoriGaleriju(id){
+
+    document.getElementById(id).style.display="block";
+
+    document.body.style.overflow="hidden";
+
+}
+
+function zatvoriGaleriju(id){
+
+    document.getElementById(id).style.display="none";
+
+    document.body.style.overflow="auto";
+
+}
