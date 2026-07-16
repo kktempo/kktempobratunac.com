1f5832083d7c06955170122301aa5cea2e72c163
KK TEMPO BRATUNAC <davidiliic996@gmail.com>
2026-07-16 08:57:23 +0200

    Add files via upload

--- selekcije.js
+++ selekcije.js
@@ -1,32 +1,7 @@
-// OTVARANJE GALERIJE
-function otvoriGaleriju(id) {
-    document.getElementById(id).style.display = "block";
-    document.body.style.overflow = "hidden";
-}
-
-// ZATVARANJE GALERIJE
-function zatvoriGaleriju(id) {
-    document.getElementById(id).style.display = "none";
-    document.body.style.overflow = "auto";
-}
-
-// Zatvaranje klikom van slike
-document.querySelectorAll(".popup-galerija").forEach(function(galerija){
-
-    galerija.addEventListener("click", function(e){
-
-        if(e.target === galerija){
-            galerija.style.display = "none";
-            document.body.style.overflow = "auto";
-        }
-
-    });
-
-});
-
 function otvoriGaleriju(id){
 
     document.getElementById(id).style.display="block";
+    document.body.style.overflow="hidden";
 
 }
 
@@ -34,5 +9,22 @@ function otvoriGaleriju(id){
 function zatvoriGaleriju(id){
 
     document.getElementById(id).style.display="none";
+    document.body.style.overflow="auto";
 
 }
+
+
+document.querySelectorAll(".popup-galerija").forEach(function(galerija){
+
+    galerija.addEventListener("click",function(e){
+
+        if(e.target === galerija){
+
+            galerija.style.display="none";
+            document.body.style.overflow="auto";
+
+        }
+
+    });
+
+});
